8af514f42d1574f4ace512ec8f93c6a8de99bfc3
Eve Leleka <ievgeniia8705@gmail.com>
2023-08-04 01:32:38 -0400

    Homework for lesson 20 added

---
+++ Lesson_20/src/main/java/com/hillel/lesson20/Calculator.java
@@ -0,0 +1,20 @@
+package com.hillel.lesson20;
+
+public class Calculator {
+
+    public static int getSum(int a, int b) {
+        return a + b;
+    }
+
+    public static int getDiff(int a, int b) {
+        return a - b;
+    }
+
+    public static int getProduct(int a, int b) {
+        return a * b;
+    }
+
+    public static int getQuotient (int a, int b) {
+        return a / b;
+    }
+}
